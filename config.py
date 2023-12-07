de855e1f015adbe8cb4f89eaaf1f021068c7e8ea
Your Name <you@example.com>
2023-12-07 13:41:49 +0100

    Muestras

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
 data_folder = "data"
 graph_folder = "graph"
 
-actividades = {"prueba_act":0,"identify_gate":1,"straight_ahead":2}
+actividades = {"prueba_act":0,
+               "identify_gate":1,
+               "straight_ahead":2,
+               "normal_stop":3,
+               "emergency_stop":4,
+               "communication":5,
+               "slow_down":6}
 usuarios = {"prueba_usu":0,
 			"javi":1,
 			"rober":2}
